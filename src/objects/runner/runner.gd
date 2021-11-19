e566e77cdf389124077f7ba2fe6ac2a8dc56ae68
Joe-noh <goflb.jh@gmail.com>
2021-11-19 20:28:25 +0900

    use apply_central_impulse

--- src/objects/runner/runner.gd
+++ src/objects/runner/runner.gd
@@ -1,20 +1,8 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed('ui_up'):
 		jump()
 
 func jump():
-	apply_impulse(Vector2.ZERO, Vector2(0, -500))
+	apply_central_impulse(Vector2(0, -500))
