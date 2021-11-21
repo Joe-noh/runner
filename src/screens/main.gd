a1a4fcb6962616751cc8aa8096a027117d1e3a59
Joe-noh <goflb.jh@gmail.com>
2021-11-21 20:08:32 +0900

    connect on gdscript

--- src/screens/main.gd
+++ src/screens/main.gd
@@ -8,7 +8,11 @@ onready var Bird = load("res://src/objects/bird/bird.tscn")
 
 func _ready():
 	rng.randomize()
+
 	$Restart.hide()
+	$Restart.connect('clicked', self, '_on_restart_clicked')
+
+	$Runner.connect('killed', self, '_on_runner_killed')
 
 func _process(delta):
 	elapsed += (10 + spawned * 0.1) * delta
@@ -24,11 +28,12 @@ func _process(delta):
 		spawned += 1
 		elapsed = 0
 
-	if GameState.is_game_over:
-		$Restart.show()
-
-func _on_Restart_clicked():
+func _on_restart_clicked():
 	if GameState.is_game_over:
 		$Restart.hide()
 		GameState.restart()
 		get_tree().reload_current_scene()
+
+func _on_runner_killed():
+	GameState.game_over()
+	$Restart.show()
