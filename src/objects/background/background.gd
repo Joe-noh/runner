99589946b6de826ce10238bd1e9701396e612e4a
Joe-noh <goflb.jh@gmail.com>
2021-11-21 20:20:12 +0900

    prefix underscore to script private variables

--- src/objects/background/background.gd
+++ src/objects/background/background.gd
@@ -1,10 +1,10 @@
 extends Sprite
 
 const VELOCITY = -200
-onready var width = texture.get_width() * scale.x
+onready var _width = texture.get_width() * scale.x
 
 func _process(delta):
 	position.x += VELOCITY * delta
 
-	if (position.x < -width):
-		position.x += width * 2
+	if (position.x < -_width):
+		position.x += _width * 2
--- src/screens/main.gd
+++ src/screens/main.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-var elapsed = 0
-var spawned = 0
-var rng = RandomNumberGenerator.new()
+var _elapsed = 0
+var _spawned = 0
+var _rng = RandomNumberGenerator.new()
 
 onready var Bird = load("res://src/objects/bird/bird.tscn")
 
 func _ready():
-	rng.randomize()
+	_rng.randomize()
 
 	$Restart.hide()
 	$Restart.connect('clicked', self, '_on_restart_clicked')
@@ -15,18 +15,18 @@ func _ready():
 	$Runner.connect('killed', self, '_on_runner_killed')
 
 func _process(delta):
-	elapsed += (10 + spawned * 0.1) * delta
+	_elapsed += (10 + _spawned * 0.1) * delta
 
-	if elapsed > 10:
+	if _elapsed > 10:
 		var bird = Bird.instance()
-		var y = rng.randi_range(10, 760)
+		var y = _rng.randi_range(10, 760)
 		bird.init(Vector2(1300, y), 2.0)
 		bird.add_to_group('birds')
 
 		add_child(bird)
 
-		spawned += 1
-		elapsed = 0
+		_spawned += 1
+		_elapsed = 0
 
 func _on_restart_clicked():
 	if GameState.is_game_over:
