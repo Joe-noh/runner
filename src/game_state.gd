d7cbb35d14f665579db0ed1a88461bf7c26ea90b
Joe-noh <goflb.jh@gmail.com>
2022-05-13 21:33:05 +0900

    white square moves around

--- src/game_state.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var is_game_over = false
-
-func game_over():
-	is_game_over = true
-
-func restart():
-	is_game_over = false
--- src/objects/restart_button/restart_button.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-signal clicked
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			emit_signal('clicked')
